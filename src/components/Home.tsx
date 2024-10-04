54141beea22d1ba1a19236e5054e77096f241bed
Francisco Menezes <franciscomenezesbh@gmail.com>
2024-10-04 17:17:06 -0300

    page home

---
+++ src/components/Home.tsx
@@ -0,0 +1,39 @@
+import { HiArrowNarrowRight } from "react-icons/hi"
+import { TypeAnimation } from 'react-type-animation';
+
+
+
+
+export const Home = () => {
+  return (
+    <div id='home' className='w-full h-screen'>
+    {/* Container */}
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <p className='text-lime-500 font-semibold'>Hi, my name is</p>
+      <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        Francisco Menezes
+      </h1>
+      <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        
+        <TypeAnimation
+            sequence={["I'm a Full Stack Developer.", 1000]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+      </h2>
+      <p className='text-[#8892b0] py-4 max-w-[700px]'>
+      Sou um desenvolvedor full-stack especializado em construir experiências digitais excepcionais. Atualmente, estou focado em construir aplicativos web full-stack responsivos.
+      </p>
+      <div>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-lime-600 hover:border-lime-600'>
+          View Work
+          <span className='group-hover:rotate-90 duration-300'>
+            <HiArrowNarrowRight className='ml-3 ' />
+          </span>
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaLinkedin, FaGithub } from 'react-icons/fa'
 import imageLogo from '../assets/logo.png'
 import { useState } from 'react'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { HiOutlineMail } from 'react-icons/hi'
 
 
 
@@ -14,13 +16,13 @@ const handelClickNav = () => {
 }
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4  bg-slate-900">
         <div>
           <img src={imageLogo} alt="logo image" style={{width: '80px'}} />
         </div> 
        {/* desktop */}
    
-          <ul className='hidden md:flex gap-4'>
+          <ul className='hidden md:flex'>
             <li>Home</li>
             <li>Sobre</li>
             <li>Skils</li>
@@ -41,9 +43,43 @@ const handelClickNav = () => {
             <li className='py-6 text-4xl'>Contatos</li>
           </ul> 
        {/* social icons */}  
-        <div className='hidden'>
-
-        </div>
+       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul className=''>
+          <li className='w-40 h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+           <a
+              className='flex justify-between items-center w-full'
+              href='https://www.linkedin.com/in/francisco-menezes-875a0a36/'
+              target="_blank"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-40 h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-40 h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-ful'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-40 h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
