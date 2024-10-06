fa0458b44510a004b2be0b6a3afb471f645acb42
Francisco Menezes <franciscomenezesbh@gmail.com>
2024-10-06 09:17:24 -0300

    page contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import { Projetos } from "./components/Projetos";
@@ -12,6 +13,7 @@ export default function App() {
     <About />
     <Skills />
     <Projetos />
+    <Contact />
   </div>
 
   )
---
+++ src/components/Contact.tsx
@@ -0,0 +1,18 @@
+
+
+export const Contact = () => {
+  return (
+    <section id='contact' className='w-full h-screen flex justify-center items-center p-4'>
+        <form action={import.meta.env.VITE_GETFORM_URL} method="POST" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-lime-600'>Contact</p>       
+            </div>
+            <input className='bg-transparent outline-none focus:border-lime-700 p-2 rounded-md border border-slate-600' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-transparent outline-none focus:border-lime-700 rounded-md border border-slate-600' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-transparent outline-none focus:border-lime-700 p-2 rounded-md border border-slate-600' name="message" rows={5} placeholder='Message'></textarea>
+            <button className='text-white border hover:bg-lime-600 hover:border-lime-600 px-4 py-3 my-8 mx-auto flex items-center'>Enviar</button>
+        </form>
+    </section>
+  )
+}
+
